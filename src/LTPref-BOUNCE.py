2cf3c7a44433b1ebf2b7711aa5e6d5e06781e18c
yizheng-hub <yizheng.li@outlook.com>
2025-10-08 21:41:56 +0800

    Update dataset

--- src/LTPref-BOUNCE.py
+++ src/LTPref-BOUNCE.py
@@ -69,7 +69,7 @@ def contains_word_match(text: str, keywords: list[str]) -> bool:
 
 # --- Global Configuration Parameters ---
 # File Paths based on the new folder structure
-PROCESSED_DATA_INPUT_PATH = "../data/processed_thermal_comfort_data.csv"
+PROCESSED_DATA_INPUT_PATH = "../data/CTCD_processed_thermal_comfort_data.csv"
 # or use ASHRAE_cleaned_OFFICE_for_simulation.csv
 
 # NLI Model Paths
